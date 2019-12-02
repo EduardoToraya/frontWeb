de7ab238d1de5dc5bc4791790b5251748074e4e4
EduardoToraya <eduar_ts1997@hotmail.com>
2019-12-02 13:13:01 -0600

    a

--- js/artist.js
+++ js/artist.js
@@ -25,7 +25,7 @@ function loadArtists() {
         console.log(data[i].name)
         // algo asi:
         // addArtist(data[i]._id, data[i].description, data[i].completed)
-        let newHTML = `<li><input  name="artist" value="2"><span> ${data[i].name} </span></li>`
+        let newHTML = `<li><span> ${data[i].name} </span></li>`
 
         $("#artist-list").append(newHTML)
       }
